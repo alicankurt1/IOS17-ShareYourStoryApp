120ff7666aea498e0f72871b9a6951ffe058f678
Alican Kurt <kurta15@itu.edu.tr>
2021-08-28 14:18:44 +0300

    Initial Commit

---
+++ ShareYourStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareYourStory
+//
+//  Created by Alican Kurt on 28.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
